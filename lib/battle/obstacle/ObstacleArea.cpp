fbf79e113044ac33136301f3b8d04a7f14e21623
FeniksFire <feniks_fire@o2.pl>
2017-07-29 14:57:18 +0200

    Creating ObstacleArea class
    Create a new class that will handling obstacle area, fixing arithmetic of
    old system for moving area to some position depending on the old
    position, also now absolute obstacle can be moved like usual.
    Making unit test for that class.

---
+++ lib/battle/obstacle/ObstacleArea.cpp
@@ -0,0 +1,52 @@
+/*
+ * ObstacleArea.cpp, part of VCMI engine
+ *
+ * Authors: listed in file AUTHORS in main folder
+ *
+ * License: GNU General Public License v2.0 or later
+ * Full text of license available in license.txt file, in main folder
+ *
+ */
+
+#include "StdInc.h"
+#include "ObstacleArea.h"
+
+void ObstacleArea::addField(BattleHex field)
+{
+	if(!vstd::contains(area, field))
+		area.push_back(field);
+}
+
+void ObstacleArea::setArea(std::vector<BattleHex> fields)
+{
+	area.clear();
+	for(auto &i : fields)
+		addField(i);
+}
+
+std::vector<BattleHex> ObstacleArea::getArea() const
+{
+	return area;
+}
+
+void ObstacleArea::moveAreaToField(BattleHex offset)
+{
+	if(position == offset)
+		return;
+	for(auto &field : area)
+	{
+		field.hex += offset.hex - position;
+		if(position.getY() % 2 != offset.getY() % 2){
+			if(offset.getY() % 2 == 0 && field.getY() % 2 == 1)
+				field.moveInDirection(BattleHex::RIGHT);
+			if(offset.getY() % 2 == 1 && field.getY() % 2 == 0)
+				field.moveInDirection(BattleHex::LEFT);
+		}
+	}
+	position = offset;
+}
+
+ObstacleArea::ObstacleArea()
+{
+	position = 0;
+}
---
+++ lib/battle/obstacle/ObstacleArea.h
@@ -0,0 +1,26 @@
+/*
+ * ObstacleArea.cpp, part of VCMI engine
+ *
+ * Authors: listed in file AUTHORS in main folder
+ *
+ * License: GNU General Public License v2.0 or later
+ * Full text of license available in license.txt file, in main folder
+ *
+ */
+#pragma once
+#include "../BattleHex.h"
+
+class DLL_LINKAGE ObstacleArea
+{
+	std::vector<BattleHex> area;
+public:
+	BattleHex position;
+
+	void addField(BattleHex field);
+	void setArea(std::vector<BattleHex> fields);
+	void moveAreaToField(BattleHex offset);
+
+	std::vector<BattleHex> getArea() const;
+
+	ObstacleArea();
+};
