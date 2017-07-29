39f0b81b26af8d72f0532dc01b18cab28c2536d9
FeniksFire <feniks_fire@o2.pl>
2017-07-29 14:58:57 +0200

    Move ObstacleType to separating header
    From CObstacleInstance.h to ObstacleType.h.
    Change to strong typed enum.

---
+++ lib/battle/obstacle/ObstacleType.h
@@ -0,0 +1,14 @@
+/*
+ * ObstacleType.h, part of VCMI engine
+ *
+ * Authors: listed in file AUTHORS in main folder
+ *
+ * License: GNU General Public License v2.0 or later
+ * Full text of license available in license.txt file, in main folder
+ *
+ */
+
+enum class ObstacleType
+{
+	USUAL, ABSOLUTE_OBSTACLE, QUICKSAND, LAND_MINE, FORCE_FIELD, FIRE_WALL, MOAT
+};
